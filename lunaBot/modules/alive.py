b4dd1c50bce683c88acd66926b5ae26cea188da7
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2021-12-19 21:49:36 +0700

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -7,23 +7,23 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/79efca0d4f1a9e2bdf84d.jpg"
+PHOTO = "https://telegra.ph/file/929d337895c056cc465d0.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Seira!** \n\n"
+  LUNA = "**Holla I'm Alby!** \n\n"
   LUNA += "💎 **I'm Working Properly** \n\n"
-  LUNA += "💎 **My Master : [ꜱᴇᴛʜ](https://t.me/xyzsethh)** \n\n"
+  LUNA += "💎 **My Master : [ꜱᴇᴛʜ](https://t.me/panggilajaalby)** \n\n"
   LUNA += f"💎 **Telethon Version : {tlhver}** \n\n"
   LUNA += f"💎 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Terima kasih sudah menambahkan Seira 💜**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/SeiraXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/seirasupport")]]
+  LUNA += "**Terima kasih sudah menambahkan Alby 💚**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/alby_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/albysupportgroup")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   LUNA = "✅ **bot berhasil di restart**\n\n• Admin list telah di **perbarui**"
-  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/seirasupport")]]
+  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/albysupportgroup")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
